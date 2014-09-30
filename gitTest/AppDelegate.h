94afe30fed023dbd1d7f6d055419f1e96bb7ea83
Andrew McMillin <APM@Andrews-MacBook-Pro.local>
2014-09-29 19:16:28 -0700

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Andrew McMillin on 9/29/14.
+//  Copyright (c) 2014 MartialLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Andrew McMillin on 9/29/14.
+//  Copyright (c) 2014 MartialLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
