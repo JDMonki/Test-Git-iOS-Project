a9638fe3382e05e43c458e2e43c55831bb2ef34a
Andrew McMillin <mcmillinap@gmail.com>
2014-09-29 19:39:47 -0700

    added a test class

---
+++ gitTest/MLTest.h
@@ -0,0 +1,13 @@
+//
+//  MLTest.h
+//  gitTest
+//
+//  Created by Andrew McMillin on 9/29/14.
+//  Copyright (c) 2014 MartialLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MLTest : NSObject
+
+@end
---
+++ gitTest/MLTest.m
@@ -0,0 +1,13 @@
+//
+//  MLTest.m
+//  gitTest
+//
+//  Created by Andrew McMillin on 9/29/14.
+//  Copyright (c) 2014 MartialLabs. All rights reserved.
+//
+
+#import "MLTest.h"
+
+@implementation MLTest
+
+@end
